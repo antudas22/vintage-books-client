247ff813d46ad5d8393c2e5ebf32b20d745f0d8a
antudas22 <20antu20@gmail.com>
2022-11-24 19:10:52 +0600

    Basic setup installation and Navbar completed

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import banner from '../../../media/banner.jpg'
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Banner;
